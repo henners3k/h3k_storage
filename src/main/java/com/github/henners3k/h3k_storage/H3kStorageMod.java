19e046678c480c696bf844e24afaf511e2b19873
Henners3K <henners3k@gmail.com>
2021-03-03 19:37:19 +0000

    Initial Commit

---
+++ src/main/java/com/github/henners3k/h3k_storage/H3kStorageMod.java
@@ -0,0 +1,38 @@
+package com.github.henners3k.h3k_storage;
+
+import com.github.henners3k.h3k_storage.mod.Constants;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Constants.MOD_ID)
+public class H3kStorageMod {
+
+    public H3kStorageMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event) {
+    }
+
+    private void processIMC(final InterModProcessEvent event) {
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+    }
+}
---
+++ src/main/java/com/github/henners3k/h3k_storage/mod/Constants.java
@@ -0,0 +1,7 @@
+package com.github.henners3k.h3k_storage.mod;
+
+public final class Constants {
+
+    public static final String MOD_ID = "h3k_storage";
+
+}
